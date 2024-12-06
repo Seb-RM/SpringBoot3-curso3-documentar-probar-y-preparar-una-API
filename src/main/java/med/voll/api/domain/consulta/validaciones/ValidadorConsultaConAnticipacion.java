451228afc6ffd2b26717e85a49c2b13598300995
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-12-06 17:16:51 -0300

    validaciones consulta

---
+++ src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorConsultaConAnticipacion.java
@@ -0,0 +1,27 @@
+package med.voll.api.domain.consulta.validaciones;
+
+import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class ValidadorHorarioConAnticipacion implements ValidadorCancelamientoDeConsulta {
+
+    @Autowired
+    private ConsultaRepository repository;
+
+    @Override
+    public void validar(DatosCancelamientoConsulta datos) {
+        var consulta = repository.getReferenceById(datos.idConsulta());
+        var ahora = LocalDateTime.now();
+        var diferenciaEnHoras = Duration.between(ahora, consulta.getData()).toHours();
+
+        if (diferenciaEnHoras < 24) {
+            throw new ValidacionException("¡La consulta solo puede ser cancelada con anticipación mínima de 24 horas!");
+        }
+    }
+}
---
+++ src/main/java/med/voll/api/domain/consulta/validaciones/ValidadorDeConsultas.java
@@ -0,0 +1,5 @@
+package med.voll.api.domain.consulta.validaciones;
+
+public interface ValidadorDeConsultas {
+    void validar(DatosReservaConsulta datos);
+}
